adb7a5a7e3be688354f2c07d9a2ff21842a5b441
Wyeth <wjp9@yahoo.com>
2012-09-13 15:50:42 -0400

    Fixed Grid Class

--- engine/Grid.py
+++ engine/Grid.py
@@ -4,8 +4,13 @@ class Grid():
     # Constructor. Pass in the width and height of the grid.
     def __init__(self, width, height, number):
 
+        self.grid = []
         self.grid_width = width
         self.grid_height = height
         self.init_number = number
 
-
+    def generate_grid(self):
+        for row in range(self.grid_height):
+            self.grid.append([])
+            for column in range(self.grid_width):
+                self.grid[row].append(0)
